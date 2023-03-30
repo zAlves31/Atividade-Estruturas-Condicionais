a3571eb3e3d7f3374e9d167272c296aad0d47fe0
João Victor Alves Bautista <52546124870@info.local>
2023-03-30 10:50:27 -0300

    Atividade-Estruturas-Condicionais

---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"Insira quantidade de maçãs");
+float maçãs = float.Parse(Console.ReadLine());
+
+
+if (maçãs < 11)
+{
+    maçãs = (Math.maçãs * 0,30);
+    Console.WriteLine($"Resultado");   
+}
+else if (maçãs > 12)
+{
+    maçãs = (Math.maçãs * 0,25);
+    Console.WriteLine($"Resultado");    
+}
