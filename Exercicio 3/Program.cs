9b31b2eab2fffe121427efd5ba13a0cd6f28a2e5
João Victor Alves Bautista <52546124870@info.local>
2023-03-31 10:12:24 -0300

    Atividade-Estruturas-Condicionais

--- Exercicio 3/Program.cs
+++ Exercicio 3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Insira a medida1");
+float lado1 =float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira a medida2");
+float lado2 =float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Insira a medida3");
+float lado3 =float.Parse(Console.ReadLine());
+
+if(lado1 == lado2 && lado2 == lado3)
+{
+    Console.WriteLine($" Triângulo Equilátero "); 
+}
+
+else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+{
+    Console.WriteLine($" Triângulo Isoscele "); 
+}
+
+else if (lado1 != lado2 && lado2 != lado3)
+{
+    Console.WriteLine($" Triângulo Escaleno "); 
+}
--- Exercicio 5/Program.cs
+++ Exercicio 5/Program.cs
@@ -1,14 +1,14 @@
 ﻿Console.WriteLine($"Insira quantidade de maçãs");
 float maçãs = float.Parse(Console.ReadLine());
 
+if (maçãs <= 11)
+{ 
+     Console.WriteLine(maçãs * 0.30);
+}
+else if (maçãs >= 12)
+{
+    Console.WriteLine(maçãs * 0.25);   
+}
 
-if (maçãs < 11)
-{
-    maçãs = (Math.maçãs * 0,30);
-    Console.WriteLine($"Resultado");   
-}
-else if (maçãs > 12)
-{
-    maçãs = (Math.maçãs * 0,25);
-    Console.WriteLine($"Resultado");    
-}
+
+
